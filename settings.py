a19623fd60f19f6c2e15aa22a939e78199abc413
Ryan Cole <ryan@localhost.localdomain>
2020-01-27 21:08:17 -0500

    Push up files
    Signed-off-by: Ryan Cole <ryan@localhost.localdomain>

---
+++ settings.py
@@ -0,0 +1,23 @@
+# List of the Meter IDs to watch
+# Use empty brackets to read all meters - []
+# List may contain only one entry - [12345678]
+# or multiple entries - [12345678, 98765432, 12340123]
+WATCHED_METERS = [12647265, 55297537, 1834435648]
+
+# MQTT Server settings
+# MQTT_HOST needs to be a string
+# MQTT_PORT needs to be an int
+# MQTT_USER needs to be a string
+# MQTT_PASSWORD needs to be a string
+# If no authentication, leave MQTT_USER and MQTT_PASSWORD empty
+MQTT_HOST = '127.0.0.1'
+MQTT_PORT = 1883
+MQTT_USER = ''
+MQTT_PASSWORD = ''
+
+# path to rtlamr
+# CHANGE THIS
+RTLAMR = '/var/home/ryan/Development/Go/bin/rtlamr'
+
+# path to rtl_tcp
+RTL_TCP = '/usr/bin/rtl_tcp'
